f0f1e6a849ef6f07190f0da18f8221310175c92a
Kabir Sewani <kabir.sewani@gmail.com>
2022-11-29 21:33:51 +0000

    implimented Express body parsing, added routing for form POST, implimented generateRandomString function

--- express_server.js
+++ express_server.js
@@ -2,15 +2,35 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+//Express Settings
 app.set("view engine", "ejs"); // use ejs templating
+app.use(express.urlencoded({ extended: true }));
 
 
+// Database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-//Main
+//Functions
+
+function generateRandomString () {
+   let rString = '';
+   let validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+   for (let x=0; x < 6; x++) {
+    rString += validChars.charAt(Math.floor(Math.random() * validChars.length));
+   }
+   return rString
+}
+
+generateRandomString();
+
+
+
+//Routing
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -19,6 +39,19 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
+
+
 
 app.get("/urls/:id", (req, res) => {
   console.log(urlDatabase)
