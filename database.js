533e9ddb4e3e989c9db05546d079811835ac7225
Kabir Sewani <kabir.sewani@gmail.com>
2022-12-09 01:51:28 +0000

    moved users and urldatabase from helpers.js to database.js

---
+++ database.js
@@ -0,0 +1,28 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "123",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+module.exports = {
+  users, 
+  urlDatabase
+}
--- helpers.js
+++ helpers.js
@@ -1,28 +1,7 @@
 const bcrypt = require("bcryptjs");
 
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-  },
-};
-
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "123",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
+//Import User and URL Database
+const { users, urlDatabase } = require("./database.js");
 
 //Functions
 function generateRandomString() {
